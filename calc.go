ef7cdf618c73e957d593c6f2fe3363ba95e4f105
Farid Murzone <faridmurzone@gmail.com>
2020-05-09 21:52:09 -0300

    added struct, receiver and functions...

--- calc.go
+++ calc.go
@@ -8,32 +8,50 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("Write an addition and press (enter): ")
+// struct
+type calc struct{}
+
+// Receiver function
+func (calc) operate(input string, operator string) int {
+	cleanInput := strings.Split(input, operator)
+	operator1 := parser(cleanInput[0])
+	operator2 := parser(cleanInput[1])
+	switch operator {
+	case "+":
+		fmt.Println("Add: ", operator1+operator2)
+		return operator1 + operator2
+	case "-":
+		fmt.Println("Sustract: ", operator1-operator2)
+		return operator1 - operator2
+	case "*":
+		fmt.Println("Multiply: ", operator1+operator2)
+		return operator1 / operator2
+	case "/":
+		fmt.Println("Division: ", operator1/operator2)
+		return operator1 * operator2
+	default:
+		fmt.Println("Operation not supported")
+		return 0
+	}
+}
+
+func parser(input string) int {
+	operator, _ := strconv.Atoi(input)
+	return operator
+}
+
+func readInput() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	operation := scanner.Text()
-	fmt.Println("You wrote: ", operation)
-	operator := "+"
-	values := strings.Split(operation, operator)
-	fmt.Println("Split: ", values)
-	fmt.Println("Concat: ", values[0]+values[1])
-	operator1, err1 := strconv.Atoi(values[0])
-	operator2, err2 := strconv.Atoi(values[1])
-	if err1 != nil || err2 != nil {
-		fmt.Println("Errores: ", err1, err2)
-	} else {
-		switch operator {
-		case "+":
-			fmt.Println("Add: ", operator1+operator2)
-		case "-":
-			fmt.Println("Sustract: ", operator1-operator2)
-		case "*":
-			fmt.Println("Multiply: ", operator1+operator2)
-		case "/":
-			fmt.Println("Division: ", operator1/operator2)
-		default:
-			fmt.Println("Operation not supported")
-		}
-	}
+	return scanner.Text()
+}
+
+func main() {
+	fmt.Println("Write an addition and press (enter): ")
+	operation := readInput()
+	fmt.Println("Which operation is this (+,-,*,/): ")
+	operationType := readInput()
+	c := calc{}
+	fmt.Println(c.operate(operation, operationType))
+
 }
