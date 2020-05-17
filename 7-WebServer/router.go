6cccf1823b21b82e35f0ba4370ecfa0489774406
Farid Murzone <faridmurzone@gmail.com>
2020-05-17 18:53:14 -0300

    Added methods

--- 7-WebServer/router.go
+++ 7-WebServer/router.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 type Router struct {
-	rules map[string]http.HandlerFunc
+	rules map[string]map[string]http.HandlerFunc
 }
 
 func NewRouter() *Router {
 	return &Router{
-		rules: make(map[string]http.HandlerFunc),
+		rules: make(map[string]map[string]http.HandlerFunc),
 	}
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	handler, methodExist, exist := r.FindHandler(request.URL.Path, request.Method)
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if !methodExist {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	handler(w, request)
 }
--- 7-WebServer/server.go
+++ 7-WebServer/server.go
@@ -14,6 +14,28 @@ func NewServer(port string) *Server {
 	}
 }
 
+func (s *Server) Handle(method string, path string, handler http.HandlerFunc) {
+	_, exist := s.router.rules[path]
+	if !exist {
+		s.router.rules[path] = make(map[string]http.HandlerFunc)
+	}
+	s.router.rules[path][method] = handler
+}
+
+func (r *Router) FindHandler(path string, method string) (http.HandlerFunc, bool, bool) {
+	_, exist := r.rules[path]
+	handler, methodExist := r.rules[path][method]
+	return handler, methodExist, exist
+}
+
+// ...Middleware could be many parameters
+func (s *Server) AddMiddleware(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
 func (s *Server) Listen() error {
 	http.Handle("/", s.router)
 	err := http.ListenAndServe(s.port, nil)
