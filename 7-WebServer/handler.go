37a18c40e84b0066678a7f8a494b0ebfb88a9092
Farid Murzone <faridmurzone@gmail.com>
2020-05-19 23:31:23 -0300

    Added response in json

--- 7-WebServer/handler.go
+++ 7-WebServer/handler.go
@@ -25,3 +25,21 @@ func PostRequest(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(metadata["name"])
 	fmt.Fprintf(w, "Payload: %v\n", metadata)
 }
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+	fmt.Fprintf(w, "Payload: %v\n", user)
+}
